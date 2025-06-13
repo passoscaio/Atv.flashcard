1243437b6cff50f3de9d6ae4471d0d99d54da4f8
passoscaio <caiofpassos09@gmail.com>
2025-06-13 08:48:25 -0300

    JavaScript Atualizado

---
+++ perguntas.js
@@ -0,0 +1,57 @@
+criarCartao (
+    'Jogos',
+    'Qual jogo começa com um caminhão gigante?',
+    'Sonic Adventure 2'
+
+)
+criarCartao (
+     'Jogos',
+    'Qual jogo Sonic precisa salvar as Wisps?',
+    'Sonic Colors'
+
+)
+criarCartao (
+     'Jogos',
+    'Qual jogo reúne as duas versões de Sonic?',
+    'Sonic Generations'
+
+)
+criarCartao (
+    'Filmes',
+    'Qual nome Gandalf recebe a partir de Duas Torres?',
+    'Mithrandir'
+)
+criarCartao (
+     'Livro',
+    'Quem é o vilão do primeiro livro de Percy Jackson?',
+    'Luke'
+
+)
+criarCartao (
+     'Filmes',
+    'Quantos Oscars Senhor dos Anéis ganhou ao todo?',
+    '17 Oscars'
+
+)
+criarCartao (
+    'sonic',
+    'Qual jogo começa com um caminhão gigante',
+    'Sonic Adventure 2'
+
+)
+criarCartao (
+     'Sonic',
+    'Qual jogo começa com um caminhão gigante',
+    'Sonic Adventure 2'
+)
+criarCartao (
+    'Sonic',
+    'Qual jogo começa com um caminhão gigante',
+    'Sonic Adventure 2'
+)
+criarCartao (
+     'Sonic',
+    'Qual jogo começa com um caminhão gigante',
+    'Sonic Adventure 2'
+
+)
