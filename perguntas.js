831fc13faf572b36abb1c6bc09e3e78c0721b477
passoscaio <caiofpassos09@gmail.com>
2025-07-04 14:30:22 -0300

    Modifiquei as perguntas e cores

--- perguntas.js
+++ perguntas.js
@@ -1,68 +1,72 @@
-criarCartao (
+criaCartao(
     'Português',
-    'O que é uma proparoxítona?',
-    'Português',
-    'Uma proparoxítona é uma palavra cuja sílaba tônica (aquela pronunciada com mais intensidade) é a antepenúltima.'
-
+    'Qual é a classe gramatical da palavra "feliz"?',
+    'Adjetivo'
 )
-criarCartao (
-     'Matemática',
+
+criaCartao(
+    'Matemática',
     'Qual é a fórmula do Teorema de Pitágoras?',
     'a²=b²+c²'
-
 )
-criarCartao (
-     'História',
+
+criaCartao(
+    'História',
     'Qual foi o último czar que a Rússia teve  antes da Revolução Russa?',
     'Czar Nicolau II'
-
 )
-criarCartao (
+
+criaCartao(
     'Geografia',
     'Qual é a cidade mais populosa do mundo?',
     'Tokyo'
 )
-criarCartao (
-     'Filosofia',
+
+criaCartao(
+    'Filosofia',
     'Quem disse: "Penso, logo existo" ?',
     'René Descartes'
-
 )
-criarCartao (
-     'Sociologia',
+
+criaCartao(
+    'Sociologia',
     'O que é cultura para a Sociologia?',
     'Um conjunto de valores, normas e práticas sociais compartilhadas'
-
 )
-criarCartao (
+
+criaCartao(
     'Física',
     'Qual é a fórmula da velocidade média?',
     'Vm = ΔS / Δt'
+)
 
+criaCartao(
+    'Química',
+    'O que é uma reação de neutralização?',
+    'É a reação entre um ácido e uma base, formando sal e água'
 )
-criarCartao (
-     'Química',
-    ' O que é uma reação de neutralização?',
-    ' É a reação entre um ácido e uma base, formando sal e água'
-)
-criarCartao (
+
+criaCartao(
     'Biologia',
     'Onde ocorre a fotossíntese?',
-    ' Nos cloroplastos das células vegetais'
+    'Nos cloroplastos das células vegetais'
 )
-criarCartao (
-     'Inglês',
+
+criaCartao(
+    'Inglês',
     'Qual o passado de "go" em inglês?',
     'Went'
-
 )
 
+criaCartao(
+    'Biologia',
+    'Qual órgão do corpo humano é responsável por bombear o sangue?',
+    'O coração.'
+)
 
-
-
-
-
-
-
-
+criaCartao(
+    'Geografia',
+    ' O que é êxodo rural?',
+    'É o deslocamento de pessoas do campo para as cidades.'
+)
 
